09709a4707962a0b890cc0505dc2b1d2025cdef7
thatrobotguy <andrewtheschueler@gmail.com>
2018-09-01 15:19:12 -0400

    Adding the protected variable keyword at the top.

--- ConnectNth/src/Utilities/StateTree.java
+++ ConnectNth/src/Utilities/StateTree.java
@@ -22,10 +22,10 @@ import java.lang.Math;
 public abstract class StateTree
 {
     public int rows, columns, winNumber, turn; // board parameters
-    boolean pop1, pop2; // true if the player has used their pop move
-    int[][] boardMatrix; // matrix representing the board (0 = empty, 1 = player1, 2 = player2)
-    StateTree parent; // parent state
-    ArrayList<StateTree> children; // list of children states
+    protected boolean pop1, pop2; // true if the player has used their pop move
+    protected int[][] boardMatrix; // matrix representing the board (0 = empty, 1 = player1, 2 = player2)
+    protected StateTree parent; // parent state
+    protected ArrayList<StateTree> children; // list of children states
     private PrintStream out = null;
 
 

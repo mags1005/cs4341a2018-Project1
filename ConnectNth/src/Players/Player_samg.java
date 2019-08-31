9e4f36c4bb5a43778c6436838562c4585137baee
Maggie Goodwin <magoodwin@wpi.edu>
2019-08-31 14:17:59 -0400

    added getMove method and default constructor

--- ConnectNth/src/Players/Player_samg.java
+++ ConnectNth/src/Players/Player_samg.java
@@ -1,4 +1,17 @@
 package Players;
 
-public class Player_samg {
+import Utilities.Move;
+import Utilities.StateTree;
+
+public class Player_samg extends Player {
+
+
+    public Player_samg(String n, int t, int l) {
+        super(n, t, l);
+    }
+
+    public Move getMove(StateTree state) {
+        return new Move(false, 5);
+    }
+
 }
